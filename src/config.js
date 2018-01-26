f4bc8c690f0b950429892d0998abcdcf23f6f696
Eric Dupertuis <dupertuis.eric@gmail.com>
2018-01-26 18:27:13 +0100

    convert to spaces 4 indent

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  gameWidth: 760,
-  gameHeight: 400,
-  localStorageName: 'phaseres6webpack'
+    gameWidth: 1024,
+    gameHeight: 768,
+    localStorageName: 'phaseres6webpack'
 }
