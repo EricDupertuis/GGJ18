0261fe831c1f472911e7e10a4d30925d54e0874d
Eric Dupertuis <dupertuis.eric@gmail.com>
2018-01-26 21:18:09 +0100

    world bounds for player

--- src/sprites/Player.js
+++ src/sprites/Player.js
@@ -2,8 +2,16 @@ import Phaser from 'phaser'
 
 export default class extends Phaser.Sprite {
     constructor({ game, x, y, asset }) {
-        super(game, x, y, asset)
-        this.anchor.setTo(0.5)
+        super(game, x, y, asset);
+
+        this.anchor.setTo(0.5);
+        this.enableBody = true;
+        this.physicsBodyType = Phaser.Physics.ARCADE;
+        this.game.physics.enable(this, Phaser.Physics.ARCADE);
+
+        this.physicsBodyType = Phaser.Physics.ARCADE;
+        this.enableBody = true;
+        this.body.collideWorldBounds = true;
     }
 
     update() {
