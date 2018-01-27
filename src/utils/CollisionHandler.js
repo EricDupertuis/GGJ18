8e75a1300c8d4fd51d836fcab57e44f4e12c9c35
Eric Dupertuis <dupertuis.eric@gmail.com>
2018-01-27 01:46:42 +0100

    fix trailing-spaces

--- src/utils/CollisionHandler.js
+++ src/utils/CollisionHandler.js
@@ -12,13 +12,13 @@ export const handlePlayerHit = (player, enemyBullet) => {
     if (player.game.time.now < player.hitCooldown) {
         return;
     }
-    
+
     --player.lives;
-    
+
     if (player.lives <= 0) {
         player.kill();
     }
-    
+
     player.hitCooldown = player.game.time.now + 1000;
     enemyBullet.kill();
 };
