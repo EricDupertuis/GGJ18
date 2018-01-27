329994d02fbfeb4cd191b511ff1269fac08562f4
Eric Dupertuis <dupertuis.eric@gmail.com>
2018-01-27 10:31:23 +0100

    WorldBounds in config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,13 @@
 export default {
     gameWidth: 1024,
     gameHeight: 768,
+    worldBoundX: 824,
+    worldBoundY: 768,
     localStorageName: 'ggj18',
     gamepadConfig: {
+
+    },
+    playerConfig: {
 
     }
 };
