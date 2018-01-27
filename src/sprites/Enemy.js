281fb8a8629db91aea28485a5e16db5312356e8b
Antoine Albertelli <antoine@antoinealb.net>
2018-01-27 17:06:19 +0100

    Basic boss behavior

--- src/sprites/Enemy.js
+++ src/sprites/Enemy.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import PatternsLibrary from './PatternsLibrary';
+import config from '../config';
 
 export default class extends Phaser.Sprite {
     constructor({ game, x, y, asset, enemyBullets, health = 50 }) {
@@ -18,10 +19,43 @@ export default class extends Phaser.Sprite {
         this.pattern = this.patternsLibrary.getPatternAtRandom();
 
         this.health = health;
+
+        this.state = 'left';
+        this.lastStateChange = 0;
     }
 
     update() {
         if (this.alive) {
+            // TODO: Maybe move based on remaining life ?
+            let targetX, targetY;
+            if (this.state === 'left') {
+                if (this.game.time.now > this.lastStateChange + 5 * 1000) {
+                    this.state = 'right';
+                    this.lastStateChange = this.game.time.now;
+                }
+                targetX = config.worldBoundX * 0.2;
+                targetY = this.body.y;
+            } else if (this.state === 'right') {
+                if (this.game.time.now > this.lastStateChange + 5 * 1000) {
+                    this.state = 'left';
+                    this.lastStateChange = this.game.time.now;
+                }
+                targetX = config.worldBoundX * 0.8;
+                targetY = this.body.y;
+            }
+
+            let dx = targetX - this.body.x;
+            let dy = targetY - this.body.y;
+
+            // Physical model. Increase kp to make the enemy move faster and
+            // increase kd to reduce the oscillations
+            let kp = 20;
+            let kd = 5;
+
+            this.body.acceleration.x = kp * dx - kd * this.body.velocity.x;
+            this.body.acceleration.y = kp * dy - kd * this.body.velocity.y;
+
+            // Shoot da gunz
             this.pattern.update();
         }
     }
