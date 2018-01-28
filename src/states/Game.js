bfcc777f91719fd32bfea23f4814bcff86b9c2ef
Antoine Albertelli <antoine@antoinealb.net>
2018-01-28 01:15:19 +0100

    fix offset of firing

--- src/states/Game.js
+++ src/states/Game.js
@@ -48,7 +48,7 @@ export default class extends Phaser.State {
         this.bullets.enableBody = true;
         this.bullets.createMultiple(300, 'redBullet');
         this.bullets.setAll('anchor.x', 0.5);
-        this.bullets.setAll('anchor.y', 1);
+        this.bullets.setAll('anchor.y', 0.5);
         this.bullets.setAll('outOfBoundsKill', true);
         this.bullets.setAll('checkWorldBounds', true);
 
